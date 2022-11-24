a91fba965115fb072d0a299e9143e6469e364e3d
dinseangmeng <dinseangmeng@gmail.com>
2022-11-25 00:00:02 +0700

    Fix overflow text and Add more feature

--- src/components/POPUP.vue
+++ src/components/POPUP.vue
@@ -17,18 +17,25 @@ const fireEdit=()=>{
 const close=()=>{
     emit('isClose')
 }
+
+const ClickeOutside=(e)=>{
+    const target=e.target;
+    if(target.className=="main"){
+        close()
+    }
+}
 </script>
 
 <template>
-    <div class="main">
-
+    <div class="main" @click="ClickeOutside">
+        
         <div id="container">
             <button @click="close"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
-            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"/>
-        </svg></button>
+                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"/>
+            </svg></button>
             <div class="Header">
                 <h1>{{Note.title}}</h1>
-
+                
             </div>
             <p class="date">{{Note.date}}</p>
             <textarea class="infor" readonly :value="Note.description"></textarea>
@@ -43,12 +50,13 @@ const close=()=>{
     background-color: #edede96f;
     font-family: 'Sono', sans-serif;
     position:fixed;
-    top: 0;
-    left: 0;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%,-50%);
     z-index: 10000000;
     
     #container{
-        width: max-content;
+        // width: max-content;
         background-color: #d5bdaf;
         // opacity: 1 !important;
         position: fixed;
@@ -58,33 +66,36 @@ const close=()=>{
         top: 50%;
         left: 50%;
         transform: translate(-50%,-50%);
-
+        
         button{
-        position: fixed;
-        top:1rem;
-        right: 1rem;
-        color: black;
-        border: none;
-        outline: none  ;
-        z-index: 9999;
-        background-color: transparent;
-        cursor: pointer;
-        svg{
-            --size:1.5rem;
-            width: var(--size);
-            height: var(--size);
-            &:hover{
-                color: rgb(255, 48, 48);
+            position: fixed;
+            top:1rem;
+            right: 1rem;
+            color: black;
+            border: none;
+            outline: none  ;
+            z-index: 9999;
+            background-color: transparent;
+            cursor: pointer;
+            svg{
+                --size:1.5rem;
+                width: var(--size);
+                height: var(--size);
+                &:hover{
+                    color: rgb(255, 48, 48);
+                }
+                transition: 200ms;
             }
-            transition: 200ms;
-        }
-    }   
+        }   
         .Header{
             display: flex;
             justify-content: space-between;
             align-items: center;
+            // width: 100%;
             h1{
                 font-size: 1.3rem;
+                // overflow-wrap: break-word;
+                // word-wrap:break-word;
             }
             .Tool{
                 display: flex;
